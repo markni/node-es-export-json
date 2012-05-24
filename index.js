a9d070aa6dfd00efa1e095f42a1b5092447b739e
Mark Ni <xixian.ni@gmail.com>
2012-05-24 00:07:14 -0700

    moving comment around, the whole block will be deleted soon anyways...

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ var
     fs = require('fs');
 
 var
-    config = process.argv[2] || "-m",
+    config = process.argv[2] || "-m", //default is export mapping
     default_index = "big-data",
     default_mapping = "magic-cards",
     default_port = "9200",
-    default_filename = "backup.json"; //default is export mapping
+    default_filename = "backup.json";
 
 var host, index_name, mapping_name, port, file_name;
 
@@ -56,6 +56,7 @@ function query_builder(size) {
 }
 
 function countCallback(response, obj) {
+    //the obj is the data we want!
     if (obj.count) {
         var count = JSON.stringify(obj.count);
 
@@ -73,8 +74,6 @@ function searchCallback(response, obj) {
         var data = JSON.stringify(obj.hits.hits);
         exportFromEs(data, file_name);
     }
-
-
 
 }
 
